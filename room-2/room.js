6576007c6d0cb917a7dfa33cfa5f79bc91b37a04
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-03-25 21:20:03 +0600

    added room-2

---
+++ room-2/room.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const holoButton = document.querySelector('.holo-button');
+    holoButton.addEventListener('click', () => {
+        window.location.href = '../views/puzzleGame-2.html';
+    });
+});
