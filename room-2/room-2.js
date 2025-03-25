ff2bf7f80b8da67b4043288909700e6e672361c5
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-03-25 21:28:17 +0600

    refactoted room-1,2

--- room-2/room-2.js
+++ room-2/room-2.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const holoButton = document.querySelector('.holo-button');
     holoButton.addEventListener('click', () => {
-        window.location.href = '../views/puzzleGame-2.html';
+        window.location.href = '../views/puzzleGame-1.html';
     });
 });
--- room-3/room.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const holoButton = document.querySelector('.holo-button');
-    holoButton.addEventListener('click', () => {
-        window.location.href = '../views/puzzleGame-3.html';
-    });
-});
